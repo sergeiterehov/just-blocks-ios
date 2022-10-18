830d739f3bf809f5bb2d22052ac63300991d67b9
Sergei Terekhov <isergeiterehov@gmail.com>
2022-10-19 00:13:01 +0300

    Initial Commit

---
+++ Just Blocks/Just_BlocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Just_BlocksApp.swift
+//  Just Blocks
+//
+//  Created by Сергей Терехов on 19.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct Just_BlocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

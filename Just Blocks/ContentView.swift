01154b85c64d496cba8484d76d087b2878c8d469
Sergei Terekhov <isergeiterehov@gmail.com>
2022-10-21 10:42:09 +0300

    Completed model and testable gameplay

--- Just Blocks/ContentView.swift
+++ Just Blocks/ContentView.swift
@@ -7,15 +7,161 @@
 
 import SwiftUI
 
-struct ContentView: View {
+let blockSize = 20
+let blockPadding = 2
+
+struct BlockView: View {
+    var block: Block
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        if (block != .Empty) {
+            Path(CGRect(x: 0 + blockPadding, y: 0 + blockPadding, width: blockSize - blockPadding * 2, height: blockSize - blockPadding * 2))
+                .fill(block == .A ? .red : .blue)
+
+            if (block == .A || block == .B) {
+                Path { path in
+                    path.move(to: CGPoint(x: blockPadding * 4, y: 0))
+                    path.addLine(to: CGPoint(x: 0, y: 0))
+                    path.addLine(to: CGPoint(x: 0, y: blockPadding * 4))
+                }
+                    .offset(x: CGFloat(blockPadding * 2), y: CGFloat(blockPadding * 2))
+                    .stroke(.white, lineWidth: CGFloat(blockPadding))
+            } else {
+                Path(CGRect(x: blockPadding * 2, y: blockPadding * 2, width: blockSize - blockPadding * 4, height: blockSize - blockPadding * 4))
+                    .fill(.white)
+            }
         }
-        .padding()
+    }
+}
+
+struct TetrominoView: View {
+    var tetromino: Tetromino
+    var offset: CGPoint
+    var rotation: Int
+
+    var body: some View {
+        ZStack {
+            let spriteVariants = tetrominoToSprite[tetromino]!
+            let sprite = spriteVariants[rotation % spriteVariants.count]
+            let size = sprite.count == 9 ? 3 : 4
+            
+            ForEach(0..<size * size, id: \.self) { index in
+                let y = index / size
+                let x = index % size
+
+                if (y + Int(offset.y) >= 0) {
+                    BlockView(block: sprite[index])
+                        .offset(
+                            x: CGFloat((x + Int(offset.x)) * blockSize),
+                            y: CGFloat((y + Int(offset.y)) * blockSize)
+                        )
+                }
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    @ObservedObject private var model = GameModel()
+
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                ZStack {
+                    Path(CGRect(x: 0, y: 0, width: 200, height: 400))
+                        .stroke(model.inProgress ? .green : .red, lineWidth: 1)
+                    
+                    ZStack {
+                        ForEach(0..<model.height * model.width, id: \.self) { index in
+                            let y = index / model.width
+                            let x = index % model.width
+
+                            BlockView(block: model.field[index])
+                                .offset(x: CGFloat(x * blockSize), y: CGFloat(y * blockSize))
+                        }
+                    }
+                    
+                    TetrominoView(tetromino: model.current, offset: CGPoint(x: model.x, y: model.y), rotation: model.rotation)
+                }
+                    .offset(x: 20, y: 20)
+                
+                ZStack {
+                    ZStack {
+                        Path(CGRect(x: 0, y: 0, width: 100, height: 110))
+                            .stroke()
+                        
+                        Text("TOP\n???\nSCORE\n\(model.score)")
+                            .position(x: 50, y: 55)
+                    }
+                        .offset(x: 0, y: 0)
+                    
+                    ZStack {
+                        Path(CGRect(x: 0, y: 0, width: 100, height: 60))
+                            .stroke()
+                        
+                        Text("LINES\n\(model.lines)")
+                            .position(x: 50, y: 30)
+                    }
+                        .offset(x: 0, y: 130)
+
+                    ZStack {
+                        Path(CGRect(x: 0, y: 0, width: 100, height: 100))
+                            .stroke(model.inProgress ? .green : .red, lineWidth: 1)
+                        
+                        TetrominoView(tetromino: model.next, offset: CGPoint(x: 0, y: 0), rotation: 0)
+                    }
+                        .offset(x: 0, y: 210)
+                    
+                    ZStack {
+                        Path(CGRect(x: 0, y: 0, width: 100, height: 60))
+                            .stroke()
+                        
+                        Text("LEVEL\n\(model.level)")
+                            .position(x: 50, y: 30)
+                    }
+                        .offset(x: 0, y: 330)
+                }
+                    .offset(x: geometry.size.width - 100 - 20, y: 20)
+                
+                ZStack {
+                    Text("<<<")
+                        .position(x: 50, y: 0)
+                        .onTapGesture {
+                            model.move(dx: -1)
+                        }
+                    
+                    Text(">>>")
+                        .position(x: 150, y: 0)
+                        .onTapGesture {
+                            model.move(dx: 1)
+                        }
+                    
+                    Text("DOWN")
+                        .position(x: 100, y: 50)
+                        .gesture(
+                            DragGesture(
+                                minimumDistance: 0,
+                                coordinateSpace: .local
+                            ).onChanged({ _ in
+                                model.softDrop = true
+                            }).onEnded({ _ in
+                                model.softDrop = false
+                            })
+                        )
+                    
+                    Text("Rotate")
+                        .position(x: geometry.size.width - 50, y: 0)
+                        .onTapGesture {
+                            if (model.inProgress) {
+                                model.rotate()
+                            } else {
+                                model.run()
+                            }
+                        }
+                }
+                    .offset(x: 0, y: geometry.size.height - 200)
+            }
+        }
     }
 }
 
---
+++ Just Blocks/GameModel.swift
@@ -0,0 +1,465 @@
+//
+//  GameModel.swift
+//  Just Blocks
+//
+//  Created by Сергей Терехов on 19.10.2022.
+//
+
+import Foundation
+
+enum Tetromino : CaseIterable {
+  case O
+  case S
+  case Z
+  case I
+  case T
+  case L
+  case J
+}
+
+enum Block {
+  case Empty
+  case A
+  case B
+  case C
+}
+
+func fillMap(type: Block, map: [Int]) -> [Block] {
+    return map.map({bit in bit == 1 ? type : Block.Empty})
+}
+
+let tetrominoToSprite: [Tetromino:[[Block]]] = [
+    .O: [
+        fillMap(type: .C, map: [
+            0, 0, 0, 0,
+            0, 1, 1, 0,
+            0, 1, 1, 0,
+            0, 0, 0, 0
+        ])
+    ],
+    .S: [
+        fillMap(type: .A, map: [
+            0, 0, 0,
+            0, 1, 1,
+            1, 1, 0
+        ]),
+        fillMap(type: .A, map: [
+            0, 1, 0,
+            0, 1, 1,
+            0, 0, 1
+        ])
+    ],
+    .Z: [
+        fillMap(type: .B, map: [
+            0, 0, 0,
+            1, 1, 0,
+            0, 1, 1
+        ]),
+        fillMap(type: .B, map: [
+            0, 0, 1,
+            0, 1, 1,
+            0, 1, 0
+        ])
+    ],
+    .I: [
+        fillMap(type: .C, map: [
+            0, 0, 0, 0,
+            0, 0, 0, 0,
+            1, 1, 1, 1,
+            0, 0, 0, 0
+        ]),
+        fillMap(type: .C, map: [
+            0, 0, 1, 0,
+            0, 0, 1, 0,
+            0, 0, 1, 0,
+            0, 0, 1, 0
+        ])
+    ],
+    .T: [
+        fillMap(type: .C, map: [
+            0, 0, 0,
+            1, 1, 1,
+            0, 1, 0
+        ]),
+        fillMap(type: .C, map: [
+            0, 1, 0,
+            1, 1, 0,
+            0, 1, 0
+        ]),
+        fillMap(type: .C, map: [
+            0, 1, 0,
+            1, 1, 1,
+            0, 0, 0
+        ]),
+        fillMap(type: .C, map: [
+            0, 1, 0,
+            0, 1, 1,
+            0, 1, 0
+        ]),
+    ],
+    .L: [
+        fillMap(type: .B, map: [
+            0, 0, 0,
+            1, 1, 1,
+            1, 0, 0
+        ]),
+        fillMap(type: .B, map: [
+            1, 1, 0,
+            0, 1, 0,
+            0, 1, 0
+        ]),
+        fillMap(type: .B, map: [
+            0, 0, 1,
+            1, 1, 1,
+            0, 0, 0
+        ]),
+        fillMap(type: .B, map: [
+            0, 1, 0,
+            0, 1, 0,
+            0, 1, 1
+        ]),
+    ],
+    .J: [
+        fillMap(type: .B, map: [
+            0, 0, 0,
+            1, 1, 1,
+            0, 0, 1
+        ]),
+        fillMap(type: .B, map: [
+            0, 1, 0,
+            0, 1, 0,
+            1, 1, 0
+        ]),
+        fillMap(type: .B, map: [
+            1, 0, 0,
+            1, 1, 1,
+            0, 0, 0
+        ]),
+        fillMap(type: .B, map: [
+            0, 1, 1,
+            0, 1, 0,
+            0, 1, 0
+        ]),
+    ],
+]
+
+let levelToFramesPerRow: [Int] = [
+    48, 43, 38, 33, 28, 23, 18, 13, 8, 6,
+    5, 5, 5, 4, 4, 4, 3, 3, 3, 2,
+    2, 2, 2, 2, 2, 2, 2, 2, 2, 1,
+]
+
+class GameModel : ObservableObject {
+    let width = 10
+    let height = 20
+    let fps = 50.0
+    
+    var clock: Timer?;
+    
+    @Published var level = 0
+    @Published var lines = 0
+    @Published var score = 0
+    
+    @Published var field: [Block] = []
+
+    @Published var inProgress = false
+    @Published var softDrop = false
+    
+    var softDropRows = 0;
+
+    @Published var current = Tetromino.S
+    @Published var next = Tetromino.T
+    
+    @Published var x = 4
+    @Published var y = 1
+    @Published var rotation = 0
+    
+    var framesToDrop = 0
+    
+    init() {
+        reset()
+
+        clock = Timer.scheduledTimer(
+            withTimeInterval: 1.0 / fps,
+            repeats: true
+        ) { timer in self.frame() }
+        
+        run()
+    }
+    
+    deinit {
+        clock?.invalidate()
+    }
+    
+    func reset() {
+        field = Array(repeating: .Empty, count: width * height)
+        x = 0
+        y = 0
+        rotation = 0
+        softDrop = false
+        level = 0
+        lines = 0
+        score = 0
+        softDropRows = 0
+        framesToDrop = levelToFramesPerRow[level]
+        
+        generateNextTetromino()
+        generateNextTetromino()
+    }
+    
+    func run() {
+        if (inProgress) {
+            return
+        }
+        
+        reset()
+
+        inProgress = true
+    }
+    
+    func stop() {
+        inProgress = false
+    }
+    
+    func move(dx: Int) {
+        if (testPosition(x: x + dx, y: y, rotation: rotation)) {
+            x += dx
+            
+            // TODO: onMove
+        }
+    }
+    
+    func rotate() {
+        if (testPosition(x: x, y: y, rotation: rotation + 1)) {
+            rotation += 1
+            
+            // TODO: onRotate
+        }
+    }
+    
+    private func frame() {
+        if (!inProgress) {
+            return
+        }
+        
+        if (softDrop || framesToDrop <= 1) {
+            if (fell()) {
+                fix()
+                
+                if (gameOver()) {
+                    stop()
+
+                    // TODO: onGameOver
+                } else {
+                    score += softDropRows
+
+                    clearLines()
+                    generateNextTetromino()
+
+                    // TODO: onDrop
+                }
+                
+                softDrop = false
+                softDropRows = 0
+            } else {
+                y += 1
+                
+                if (softDrop) {
+                    softDropRows += 1
+                }
+            }
+            
+            // Level
+            
+            let prevLevel = level
+            
+            level = Int(lines / 10)
+            
+            if (level != prevLevel) {
+                // TODO: onLevelUp
+            }
+            
+            // Continue...
+            
+            framesToDrop = levelToFramesPerRow[level]
+        } else {
+            framesToDrop -= 1
+        }
+    }
+    
+    private func getHeight() -> Int {
+        for y in 0..<height {
+            for x in 0..<width {
+                if (field[y * width + x] == .Empty) {
+                    return height - y
+                }
+            }
+        }
+        
+        return 0
+    }
+    
+    private func gameOver() -> Bool {
+        for x in 0..<width {
+            if (field[x] != .Empty) {
+                return true
+            }
+        }
+
+        return false
+    }
+    
+    private func fell() -> Bool {
+        let spriteVariants = tetrominoToSprite[current]!
+        let sprite = spriteVariants[rotation % spriteVariants.count]
+        let size = sprite.count == 9 ? 3 : 4
+        
+        for dy in 0..<size {
+            for dx in 0..<size {
+                if (sprite[dy * size + dx] != .Empty && (
+                    (
+                        y + dy + 1 >= 0 && y + dy + 1 < height
+                        && x + dx >= 0 && x + dx < width
+                        && field[(y + dy + 1) * width + x + dx] != .Empty
+                    )
+                    || y + dy + 1 == height
+                )) {
+                    return true
+                }
+            }
+        }
+
+        return false
+    }
+    
+    private func testPosition(x: Int, y: Int, rotation: Int) -> Bool {
+        let spriteVariants = tetrominoToSprite[current]!
+        let sprite = spriteVariants[rotation % spriteVariants.count]
+        let size = sprite.count == 9 ? 3 : 4
+        
+        for dy in 0..<size {
+            for dx in 0..<size {
+                if (sprite[dy * size + dx] != .Empty && (
+                    y + dy >= height
+                    || x + dx < 0
+                    || x + dx >= width
+                    || (
+                        y + dy >= 0 && y + dy < height
+                        && x + dx >= 0 && x + dx < width
+                        && field[(y + dy) * width + x + dx] != .Empty
+                    )
+                )) {
+                    return false
+                }
+            }
+        }
+
+        return true
+    }
+    
+    private func fix() {
+        let spriteVariants = tetrominoToSprite[current]!
+        let sprite = spriteVariants[rotation % spriteVariants.count]
+        let size = sprite.count == 9 ? 3 : 4
+        
+        for dy in 0..<size {
+            if (y + dy >= 0 && y + dy < height) {
+                for dx in 0..<size {
+                    if (x + dx >= 0 && x + dx < width) {
+                        let block = sprite[dy * size + dx]
+                        
+                        if (block != .Empty) {
+                            field[(y + dy) * width + x + dx] = block
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    private func clearLines() {
+        var cleared = 0
+
+        for y in 0..<height {
+            var filled = true
+            
+            for x in 0..<width {
+                if (field[y * width + x] == .Empty) {
+                    filled = false
+                    break
+                }
+            }
+            
+            if (!filled) {
+                continue
+            }
+            
+            cleared += 1
+            
+            for t in (1...y).reversed() {
+                for x in 0..<width {
+                    field[t * width + x] = field[(t - 1) * width + x]
+                }
+            }
+            
+            for x in 0..<width {
+                field[x] = .Empty
+            }
+        }
+        
+        lines += cleared
+        
+        if (cleared == 0) {
+          // nop
+        } else if (cleared == 1) {
+          score += 40 * (level + 1)
+            
+          // TODO: onClear
+        } else if (cleared == 2) {
+          score += 100 * (level + 1)
+            
+        // TODO: onClear
+        } else if (cleared == 3) {
+          score += 300 * (level + 1)
+            
+        // TODO: onClear
+        } else if (cleared >= 4) {
+          score += 1200 * (level + 1)
+            
+        // TODO: onTetris
+        }
+    }
+    
+    private func generateNextTetromino() {
+        current = next
+        
+        next = Tetromino.allCases.randomElement()!
+        
+        if (next == current) {
+            next = Tetromino.allCases.randomElement()!
+        }
+        
+        rotation = 0
+        
+        let sprite = tetrominoToSprite[current]![rotation]
+        let size = sprite.count == 9 ? 3 : 4
+        
+        for dy in 0..<size {
+            var found = false
+            
+            for dx in 0..<size {
+                if (sprite[dy * size + dx] != .Empty) {
+                    y = -dy
+                    found = true
+                    break
+                }
+            }
+            
+            if (found) {
+                break
+            }
+        }
+        
+        x = Int((width - size) / 2)
+    }
+}
